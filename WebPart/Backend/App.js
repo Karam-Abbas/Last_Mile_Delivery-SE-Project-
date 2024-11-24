5afb431139f5dff369489a76bf0c3142097f4dc9
Karam <70372614+abcd-hud@users.noreply.github.com>
2024-11-24 10:07:34 +0500

    completed all the tasks plus added an .env file plus a mongoose connection file

--- WebPart/Backend/App.js
+++ WebPart/Backend/App.js
@@ -8,7 +8,7 @@ app.use(cors({
   methods: ["GET", "POST"], // Specify allowed methods
   allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
 }));
-const Log = require("./models/logmodel"); // Import the log schema
+const Log = require("./models/logModel"); // Import the log schema
 const User = require("./models/user"); // Import the user
 app.use(express.json());
 
